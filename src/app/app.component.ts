b0bd0e7555f117494f0bbd50c529e78ec34fcb0c
HoangDH <daohuyhoang9119@gmail.com>
2023-02-26 11:15:06 +0700

    Add component and css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+import { lorem } from 'faker';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'typing-check';
+  // random_T: string[] = ['a','b','c','d'];
+  // let randomNumber = Math.floor(Math.random() * random_T.length);
+  random_Text = lorem.lines(1);
+  txt: string = '';
+
+  checkTyping(value: any) {
+    console.log(value);
+    console.log(typeof this.random_Text);
+    //convert random text to array
+    console.log(Array.from(this.random_Text));
+    console.log(Array.from(this.random_Text).length);
+  }
 }
