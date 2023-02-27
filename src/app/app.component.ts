a411973e1201e8b3d8c0f0a71352f7bfca34094b
HoangDH <daohuyhoang9119@gmail.com>
2023-02-27 22:10:48 +0700

    Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,49 @@ import { lorem } from 'faker';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  // random_T: string[] = ['a','b','c','d'];
-  // let randomNumber = Math.floor(Math.random() * random_T.length);
   random_Text = lorem.lines(1);
-  txt: string = '';
+  enterText = '';
+  flag = false;
+  activeText = 'gray';
 
-  checkTyping(value: any) {
-    console.log(value);
-    console.log(typeof this.random_Text);
-    //convert random text to array
-    console.log(Array.from(this.random_Text));
-    console.log(Array.from(this.random_Text).length);
+  onInput(input: any) {
+    console.log(input.value.split(''));
+    //get the value of random text
+    console.log(this.random_Text);
+    this.enterText = input.value;
+    //compare input to random text
+    // if (input.value === this.random_Text) {
+    //   console.log('congrats you win');
+    //   this.flag = true;
+    // } else {
+    //   console.log('not correct');
+    //   this.flag = false;
+    // }
+
+    //compare each alphabet
+    //if right => green, wrong => red, nothing => gray
+    // let inputValue = Array.from(input.value);
+    // let textValue = Array.from(this.random_Text);
+    // for (var char1 of this.random_Text) {
+    //   for (var char2 of input.value) {
+    //     if (char1 !== char2) {
+    //       this.activeText = 'red';
+    //     } else if (char1 === char2) {
+    //       this.activeText = 'green';
+    //     } else {
+    //       this.activeText = 'gray';
+    //     }
+    //   }
+    // }
+  }
+  compare(char: string, enterText: string): string {
+    if (!enterText) {
+      return 'gray';
+    }
+    if (char === enterText) {
+      return 'green';
+    } else {
+      return 'red';
+    }
   }
 }
